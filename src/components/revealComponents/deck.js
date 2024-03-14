3ed15b9243b48150287e5761f87ef9d5723f11ae
Nat Karmios <nat@karmios.com>
2024-03-14 18:34:00 +0000

    Update React rendering and Reveal.js init

--- src/components/revealComponents/deck.js
+++ src/components/revealComponents/deck.js
@@ -1,21 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Reveal from 'reveal.js';
 import PropTypes from 'prop-types';
 import revealOptions from './revealOptions';
 import wislHighlight from './wislHighlight';
 
 export default function Deck({ options, children }) {
+  const deckDivRef = useRef(null);
+  const deckRef = useRef(null);
+
   useEffect(() => {
-    Reveal.initialize({
+    if (deckRef.current) return;
+
+    deckRef.current = new Reveal(deckDivRef.current, {
       ...revealOptions,
       ...options,
       highlight: {
         beforeHighlight: (hljs) => hljs.registerLanguage('wisl', wislHighlight),
       },
     });
+
+    deckRef.current.initialize();
+
+    // eslint-disable-next-line consistent-return
+    return () => {
+      try {
+        if (deckRef.current) {
+          deckRef.current.destroy();
+          deckRef.current = null;
+        }
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.warn('Reveal.js destroy call failed.');
+      }
+    };
   });
+
   return (
-    <div className="reveal">
+    <div className="reveal" ref={deckDivRef}>
       <div className="slides">{children}</div>
     </div>
   );
